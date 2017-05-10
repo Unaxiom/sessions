3bc46c83b00c2a48197ffefa51472e88d957c305
Pratheek Adidela <apratheek@unaxiom.com>
2017-05-10 20:04:03 +0530

    Changed table function signature in methods.go. Now accepts the default schema and the database name as parameters, instead of as struct variables.

--- methods.go
+++ methods.go
@@ -1,14 +1,12 @@
 package sessions
 
 import (
-	"HR/structs"
-
 	"github.com/apratheek/schemamagic"
 	pgx "gopkg.in/jackc/pgx.v2"
 )
 
 // TableSessions creates the Sessions table
-func TableSessions(tx *pgx.Tx) *schemamagic.Table {
+func TableSessions(tx *pgx.Tx, defaultSchema string, database string) *schemamagic.Table {
 	/*
 		CREATE TABLE sessions (
 			id bigserial UNIQUE PRIMARY KEY,
@@ -18,7 +16,7 @@ func TableSessions(tx *pgx.Tx) *schemamagic.Table {
 			timestamp bigint DEFAULT EXTRACT(EPOCH FROM NOW())::bigint
 		)
 	*/
-	table := schemamagic.NewTable(schemamagic.Table{Name: "sessions", DefaultSchema: structs.DefaultSchema, Database: structs.Database, Tx: tx})
+	table := schemamagic.NewTable(schemamagic.Table{Name: "sessions", DefaultSchema: defaultSchema, Database: database, Tx: tx})
 	table.Append(schemamagic.NewColumn(schemamagic.Column{Name: "id", Datatype: "bigserial", IsPrimary: true, IsUnique: true}))
 	table.Append(schemamagic.NewColumn(schemamagic.Column{Name: "name", Datatype: "text", IsNotNull: true}))
 	table.Append(schemamagic.NewColumn(schemamagic.Column{Name: "holiday_date", Datatype: "date", IsNotNull: true}))
